146dac1f72e73f89410e253dfd7803b87aee40de
WitchingHr <matt1494@gmail.com>
2022-11-25 16:56:04 -0500

    Add hovering text change to read status

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const modal = document.querySelector('.modal-bg');
-
 const button = document.querySelector('.add');
-
 const modalScale = document.querySelector('.modal');
+const readStatus = document.querySelector('.read-status');
 
 button.onclick = function() {
     modal.style.display = 'block';
     modalScale.classList.add('scale');
-}
+};
 
 window.onclick = function(e) {
     if (e.target == modal) {
@@ -15,3 +14,30 @@ window.onclick = function(e) {
     }
 };
 
+readStatus.addEventListener('click', (e) => {
+    if (e.target.classList.contains('read')) {
+        e.target.classList.remove('read');
+        e.target.classList.add('unread');
+        e.target.textContent = 'Unread';
+        return;
+    }
+    e.target.classList.remove('unread');
+    e.target.classList.add('read');
+    e.target.textContent = 'Read';
+});
+
+readStatus.addEventListener('mouseover', (e) => {
+    if (e.target.classList.contains('read')) {
+        e.target.textContent = 'Unread';
+        return;
+    }
+    e.target.textContent = 'Read';
+});
+
+readStatus.addEventListener('mouseout', (e) => {
+    if (e.target.classList.contains('read')) {
+        e.target.textContent = 'Read';
+        return;
+    }
+    e.target.textContent = 'Unread';
+});

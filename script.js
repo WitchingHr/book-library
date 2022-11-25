aece6eb91b3cb1914dcb30f5bdde5fe356ef2e7c
WitchingHr <matt1494@gmail.com>
2022-11-25 01:46:55 -0500

    Animate modal opening

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ const modal = document.querySelector('.modal-bg');
 
 const button = document.querySelector('.add');
 
+const modalScale = document.querySelector('.modal');
+
 button.onclick = function() {
     modal.style.display = 'block';
+    modalScale.classList.add('scale');
 }
 
 window.onclick = function(e) {

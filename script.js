753757fdcf3449bc5ca363dc2ef1afeca042b327
WitchingHr <matt1494@gmail.com>
2022-11-26 22:10:00 -0500

    Implement DOM manipulator for Add Book function

--- script.js
+++ script.js
@@ -41,3 +41,76 @@ readStatus.addEventListener('mouseout', (e) => {
     }
     e.target.textContent = 'Unread';
 });
+
+const library = [];
+
+function Book(title, author, pages, read) {
+    this.title = title,
+    this.author = author,
+    this.pages = pages,
+    this.read = read
+}
+
+
+
+function addBookToLibrary() {
+    const title = document.getElementById('book-title').value;
+    const author = document.getElementById('book-author').value;
+    const pages = document.getElementById('book-pages').value;
+    const read = document.getElementsByName('read').value;
+    library.push(new Book(title, author, pages, read));
+    populate();
+    console.log(library); // remove me
+}
+
+function populate() {
+    const bookshelf = document.querySelector('.bookshelf');
+    let books = document.querySelectorAll('.book');
+    books.forEach(book => {
+        bookshelf.removeChild(book);
+    })
+    library.forEach(book => {
+        const bk = document.createElement('div');
+        bk.classList.add('book');
+        bookshelf.appendChild(bk);
+
+        const title = document.createElement('div');
+        title.classList.add('title');
+        title.textContent = book.title;
+        bk.appendChild(title);
+
+        const author = document.createElement('div');
+        author.classList.add('author');
+        author.textContent = book.author;
+        bk.appendChild(author);
+
+        const pages = document.createElement('div');
+        pages.classList.add('pages');
+        pages.textContent = `${book.pages} pages`;
+        bk.appendChild(pages);
+
+        const wrapper = document.createElement('div');
+        wrapper.classList.add('buttons-wrapper');
+        bk.appendChild(wrapper);
+
+        const read = document.createElement('button');
+        read.classList.add('read');
+        read.classList.add('book-buttons');
+        read.classList.add('read-status')
+        read.textContent = 'Read'; // FIX ME
+        wrapper.appendChild(read);
+
+        const remove = document.createElement('button');
+        remove.classList.add('remove');
+        remove.classList.add('book-buttons')
+        remove.textContent = 'Remove';
+        wrapper.appendChild(remove);
+    })
+}
+
+const submitBook = document.querySelector('.submit-book');
+
+submitBook.addEventListener('click', (e) => {
+    e.preventDefault();
+    addBookToLibrary();
+});
